fc3d566a8ff414e31b1b7c34bfd87ff637657f68
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-12-03 14:34:10 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,7 +4,7 @@ attr_accessor :email_addresses
     @email_addresses=email_addresses
   end
   def parse
-    email_addresses.split.uniq
+    email_addresses.split(" ").uniq
   end
 end
 # Build a class EmailParser that accepts a string of unformatted
